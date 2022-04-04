8e14cbe10165fb3b34475dc7cba89a7cb1e72822
Saulo <saulodetp@gmail.com>
2022-04-04 00:25:54 -0300

    com grafico andando

--- dashtest.py
+++ dashtest.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -6,11 +6,12 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv('db_publicdata.csv')
+# df = pd.DataFrame({
+#    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#    "Amount": [4, 1, 2, 2, 4, 5],
+#    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
@@ -21,11 +22,26 @@ app.layout = html.Div(children=[
         Dash: A web application framework for your data.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    html.Div(test='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Dropdown(df['nome'], value='Todos', id='dropdown-deputados'),
+    dcc.Graph(id='example-graph',  figure=fig)
 ])
+
+
+@app.callback(
+    Output('dd-output-container', 'test'),
+    Input('dropdown-deputados', 'value')
+)
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+#dfmall = dfmain.loc[dfmain['ID Loja'] == 'Shopping Morumbi']
+# df['valorDocumento'].nlargest(n=10)
