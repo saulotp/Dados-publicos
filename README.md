c565985a59ddabf50e900dd4e3c4fc8a595efcd0
Saulo <saulodetp@gmail.com>
2022-04-06 20:00:14 -0300

    comit pra rebootar

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://www.youtube.com/watch?v=FXlixv8Ieoc&ab_channel=B%C3%B3sonTreinamentos
 https://www.dataquest.io/blog/sql-insert-tutorial/
 
 https://docs.sqlalchemy.org/en/14/core/engines.html
+
+
+
--- dashtest.py
+++ dashtest.py
@@ -1,47 +1,58 @@
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.io as pio
+pio.renderers.default = "notebook_connected"
+
+
+
 
 app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv('db_publicdata.csv')
-# df = pd.DataFrame({
-#    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#    "Amount": [4, 1, 2, 2, 4, 5],
-#    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-# })
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    html.Div(test='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Dropdown(df['nome'], value='Todos', id='dropdown-deputados'),
-    dcc.Graph(id='example-graph',  figure=fig)
-])
+df = df.loc[df['nome'] == 'Abílio Santana']
+df = df[[ 'valorDocumento', 'tipoDespesa']].groupby('tipoDespesa').sum().reset_index()
+df = df.sort_values(by='valorDocumento', ascending=False)
+df = df.head(10)
 
 
-@app.callback(
-    Output('dd-output-container', 'test'),
-    Input('dropdown-deputados', 'value')
+
+fig = px.bar(df, x="tipoDespesa", y="valorDocumento", text_auto=True)
+fig.update_yaxes(visible=False, showticklabels=False)
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
 )
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
 
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for your data.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-#dfmall = dfmain.loc[dfmain['ID Loja'] == 'Shopping Morumbi']
-# df['valorDocumento'].nlargest(n=10)
